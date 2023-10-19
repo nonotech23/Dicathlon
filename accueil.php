b1ff031559c91f29944f6f05fdc56bfd6824822d
NonoTech <53663636+nonotech23@users.noreply.github.com>
2023-10-19 10:21:38 +0200

    V2

---
+++ accueil.php
@@ -0,0 +1,19 @@
+<?php require_once "./inc/head.php"; ?>
+<?php require_once "inc/header.php"; ?>
+
+
+<body>
+    <body>
+        <main id="main">
+        </main>
+        <section id="footer">
+            <footer>
+                <div class="credits">
+                    <h3>Noan DELANEAU / Baptiste Cadio  BUT MMI Lannion 2023</h3>
+                </div>
+            </footer>
+        </section>
+    </body>
+</body>
+
+</html>
---
+++ inc/header.php
@@ -0,0 +1,15 @@
+<section class="accueil">
+    <nav>
+        <a href="index.php">
+            <h2 class="logo"><img src="./IMG/dicathlon-light.png" alt="logo dicathlon"></h2>
+        </a>
+        <ul id="primary">
+            <li><a href="bonplan.php">Bon plans</a></li>
+            <li><a href="sports.php">Sports</a></li>
+            <li><a href="réserations.php">Mes réservations</a></li>
+        </ul>
+        <a href="index.php">
+            <h2 class="logo"><img src="./IMG/icons/user-light.svg" alt="user icon"></h2>
+        </a>
+    </nav>
+</section>
