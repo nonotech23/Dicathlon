215571c3df84e0b0107346fb9b2f2995e48a3894
NonoTech <53663636+nonotech23@users.noreply.github.com>
2023-12-04 15:09:12 +0100

    Update head.php

--- application/views/head.php
+++ application/views/head.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="<?php echo(base_url());?>CSS/style-sports.css">
 
     <link rel="stylesheet" href="<?php echo(base_url());?>CSS/normalize.less">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo(base_url());?>bootstrap-3.3.7/dist/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
