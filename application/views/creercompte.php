4b0ccc7881729c7bb6cd32d0bd6abe915e5781d0
Baptoutou <134270926+Baptoutou@users.noreply.github.com>
2023-12-12 11:01:36 +0100

    créer le compte

--- application/views/creercompte.php
+++ application/views/creercompte.php
@@ -1,23 +1,33 @@
 <body id="connexion-bg">
-<body id="connexion-bg">
     <main id="main-co">
         <!-- Dans votre fichier connexion.php -->
 
         <div class="connexion-container">
             <h2 class="connexion-text">Formulaire de Connexion</h2>
 
-            <form action="<?php echo(site_url("Utilisateur/verif_connexion")); ?>" method="post">
+            <form action="<?php echo(site_url("Utilisateur/creer_lecompte")); ?>" method="post">
+                <label for="prenom">Prénom :</label>
+                <input type="text" id="prenom" name="prenom" required placeholder="prenom">
+
+                <label for="nom">Nom :</label>
+                <input type="text" id="nom" name="nom" required placeholder="nom">
+
+                <label for="ddn">Date de naissance :</label>
+                <input type="date" id="ddn" name="ddn" required placeholder="ddn">
+
+                <label for="email">Adresse mail :</label>
+                <input type="text" id="email" name="email" required placeholder="email">
+
                 <label for="username">Nom d'utilisateur :</label>
                 <input type="text" id="login" name="login" required placeholder="Nom d'utilisateur">
 
                 <label for="password">Mot de passe :</label>
                 <input type="password" id="motdepasse" name="password" required placeholder="Mot de passe">
+
                 <div class="buttons-co">
                     <button type="submit">Créer mon compte</button>
                 </div>
             </form>
-
-           
 
         </div>
     </main>
@@ -28,9 +38,6 @@
             </div>
         </footer>
     </section>
-</body>
-
-</html>
 </body>
 
 </html>
